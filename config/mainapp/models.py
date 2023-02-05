3814c728ec97b0cb07d85d9320d7932bfcd6819a
k2foxspb <k2foxpsb@mail.ru>
2023-02-05 21:51:01 +0300

    lesson_4 Migrations

--- config/mainapp/models.py
+++ config/mainapp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=256, verbose_name="Title")
-    preambule = models.CharField(max_length=1024, verbose_name="Preambule")
+    title = models.CharField(max_length=255, verbose_name="Title")
+    preambule = models.CharField(max_length=1000, verbose_name="Preambule")
     body = models.TextField(blank=True, null=True, verbose_name="Body")
     body_as_markdown = models.BooleanField(default=False, verbose_name="As markdown")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
